1939ee40a4bb5fd6b5c17fca8a2bf6c60387cdcf
csjames <csjames0303@gmail.com>
2023-07-20 16:11:55 +0800

    config changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   experimental: {
     appDir: true,
@@ -12,5 +14,6 @@ const nextConfig = {
     ],
   },
 }
+
 
 module.exports = nextConfig
